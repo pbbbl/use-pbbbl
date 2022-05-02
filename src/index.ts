4485423fcc8102c95043a68390ba2a3dc80779a9
Tyler Grow <tyler.grow@mx.com>
2022-05-02 13:59:16 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
+console.log('use-pbbbl is working');
